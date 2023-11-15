828fdf552dda8b0bff6d73b823cd68d204ec38a0
Kristian Rödin <kristian.rodin@specifyit.se>
2023-11-15 18:53:10 +0100

    Test fallback on 404 when reloading with react router

--- homepage/src/routes.tsx
+++ homepage/src/routes.tsx
@@ -1,6 +1,6 @@
 import { Suspense, lazy } from 'react';
 import MainLayout from './layout/MainLayout';
-import { RouteObject } from 'react-router-dom';
+import { Navigate, RouteObject } from 'react-router-dom';
 import LoadingScreen from './LoadingScreen';
 
 const Loadable = (Component: any) => (props: JSX.IntrinsicAttributes) =>
@@ -12,26 +12,40 @@ const Loadable = (Component: any) => (props: JSX.IntrinsicAttributes) =>
 
 const Home = Loadable(lazy(() => import('./pages/Home')));
 const About = Loadable(lazy(() => import('./pages/About')));
+const PageNotFound = Loadable(lazy(() => import('./pages/PageNotFound')));
 
 const routes: RouteObject[] = [
   {
+    path: '/',
+    element: <Navigate replace to='/home' />,
+  },
+  {    
     path: '*',
     element: <MainLayout />,
-    children: [
-      {
-        path: 'about',
-        element: <About />
-      },
+    children: [      
       {
         path: 'home',
         index: true,
         element: <Home />,
       },
       {
-        path: '*',
+        path: 'about',
+        element: <About />
+      },      
+      {
+        path: '404',
         index: true,
-        element: <Home />,
+        element: <PageNotFound />,
+      },
+      {
+        path: '*',
+        element: <Navigate replace to='/404' />
       }
+      // {
+      //   path: '*',
+      //   index: true,
+      //   element: <Home />,
+      // }
     ]
   },
 ];
