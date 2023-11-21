79c96d2c050dc8262334d13475bfcd3052df1a42
Kristian Rödin <kristian.rodin@specifyit.se>
2023-11-21 20:23:40 +0100

    Test mask

--- homepage/src/pages/Home.tsx
+++ homepage/src/pages/Home.tsx
@@ -1,10 +1,50 @@
+import styled from 'styled-components';
+import code from './../assets/images/code.svg';
+import wave from './../assets/images/wave.png';
+
+const Main = styled.main`
+  display: flex;
+`;
+const Container = styled.div`
+  width: 50%;
+`;
+
+type ImageContainerProps = {
+  url: any;
+};
+
+const ImageContainer = styled.div<ImageContainerProps>`
+  width: 100%;
+
+  -webkit-mask-image: url(${(props) => props.url ?? ''});
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-size: 100%;
+  mask-image: url(${(props) => props.url ?? ''});
+  mask-repeat: no-repeat;
+  mask-size: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    overflow: hidden;
+  }
+`;
+
 const Home = () => {
   return (
     <div>
       <h2>Home Page</h2>
-      <main>
-        <p>This section contains information about...</p>
-      </main>
+      <Main>
+        <Container>
+          <p>This section contains information about...</p>
+        </Container>        
+        <Container>
+          <ImageContainer url={wave}>
+            <img src={code} className="Code-hero" alt="code" />
+          </ImageContainer>
+        </Container>
+      </Main>
     </div>
   );
 }
