9c9cb4dda70dce1488c82fc36d74a5aca22a24e1
Kristian Rödin <kristian.rodin@specifyit.se>
2023-11-21 20:24:45 +0100

    Updates

--- static/js/293.ac84921f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhomepage=self.webpackChunkhomepage||[]).push([[293],{293:(e,s,n)=>{n.r(s),n.d(s,{default:()=>a});var i=n(184);const a=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)("h2",{children:"Home Page"}),(0,i.jsx)("main",{children:(0,i.jsx)("p",{children:"This section contains information about..."})})]})}}]);
-//# sourceMappingURL=293.ac84921f.chunk.js.map
