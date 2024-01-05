7294d6b959816d7850f02fabe484c9934d045970
Kristian Rödin <kristian.rodin@specifyit.se>
2024-01-05 19:39:25 +0100

    Updates

---
+++ static/js/793.e961d1ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhomepage=self.webpackChunkhomepage||[]).push([[793],{793:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});var h=a(184);const c=()=>(0,h.jsx)("div",{children:"Tj\xe4nster"})}}]);
+//# sourceMappingURL=793.e961d1ea.chunk.js.map
